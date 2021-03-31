51e66a6721c1075713861d4862db7da45167fec3
Seth Delorme <delorme.s.m@gmail.com>
2021-03-31 18:29:09 -0500

    adds create action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <p>
+      Place Name:
+      <input v-model="newPlaceName" />
+    </p>
+    <p>
+      Place Address:
+      <input v-model="newPlaceAddress" />
+    </p>
+    <button v-on:click="createPlace()">Add Place To List</button>
+    <hr />
     <div v-for="place in places" :key="place.id">
       {{ place.name }}
       <br />
@@ -19,6 +29,8 @@ export default {
     return {
       message: "Vermont Places > Everywhere Else",
       places: [],
+      newPlaceName: "",
+      newPlaceAddress: "",
     };
   },
   created: function () {
@@ -29,6 +41,18 @@ export default {
       axios.get("/api/places").then((response) => {
         console.log("places index working", response);
         this.places = response.data;
+      });
+    },
+    createPlace: function () {
+      var params = {
+        name: this.newPlaceName,
+        address: this.newPlaceAddress,
+      };
+      axios.post("/api/places", params).then((response) => {
+        console.log("creating place", response);
+        this.places.push(response.data);
+        this.newPlaceName = "";
+        this.newPlaceAddress = "";
       });
     },
   },
