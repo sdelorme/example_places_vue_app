81f13c9860e62d53b20c42432e65cb62e5b0410e
Seth Delorme <delorme.s.m@gmail.com>
2021-03-30 21:21:59 -0500

    create index action

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="place in places" :key="place.id">
+      {{ place.name }}
+      <br />
+      {{ place.address }}
+      <hr />
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Vermont Places > Everywhere Else",
+      places: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("/api/places").then((response) => {
+        console.log("places index working", response);
+        this.places = response.data;
+      });
+    },
+  },
 };
 </script>
